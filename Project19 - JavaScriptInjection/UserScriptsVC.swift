ec1e11c6db524b3f5c7ac130dd4be01b942c4da8
Noah Pope <npope@tutanota.com>
2024-10-22 12:59:58 -0500

    adding new vc to be presented that displays user's custom scripts to be run in js extension

---
+++ Project19 - JavaScriptInjection/UserScriptsVC.swift
@@ -0,0 +1,20 @@
+//
+//  UserScriptsVC.swift
+//  Project19 - JavaScriptInjection
+//
+//  Created by Noah Pope on 10/22/24.
+//
+
+import UIKit
+
+class UserScriptsVC: UIViewController {
+
+    let tableView       = UITableView()
+    var scriptOptions   = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
